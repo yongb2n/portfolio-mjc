34645f05c5074b4bdf08668ea183d5437bbdab15
binyongbin <kyb2475@gmail.com>
2023-10-26 20:44:53 +0900

    add

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   outputDir: path.resolve(__dirname, "./docs"),
-  publicPath: "/portfolio/",
+  publicPath: "/portfolio-mjc/",
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
